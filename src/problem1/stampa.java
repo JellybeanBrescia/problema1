afa3c81dcf44b463fc810a26861170ac917d4c7a
n4rwal <r.kumar002@studenti.unibs.it>
2017-05-18 13:58:21 +0200

    aggiunta 10,100 e oridinato un pò il codice

--- src/problem1/stampa.java
+++ src/problem1/stampa.java
@@ -6,7 +6,7 @@ public class stampa {
 		
 		Risultato();
 		
-		//TODO set limit to count che roba è?
+		//TODO implementare timestamp
 		
 		if(args.length<=0){
 			System.err.println("Set limit to count");
@@ -49,17 +49,20 @@ public class stampa {
 		logtime.getDelta(LogTime.MINUTE);
 		
 	}
-	
+	//somma dei multipli di 3 e 5 minori di 1000,100,10
 	public static void Risultato() {
 		
 	int primoMultiplo = 3;
 	int secondoMultiplo = 5;
 	int mcm = primoMultiplo*secondoMultiplo;
 	int risultato = 0;
+	int risultato1 = 0;
+	int risultato2 = 0;
 	int ripetizione = 0;
-	{
-	for (int i = 1; i<=333; i++){
-		if (i<=199)
+	int ripetizione1 = 0;
+	
+	{ for (int i = 1; i<=333; i++){ // 333 è il numero di volte che i multipli di 3 sono presenti sotto 1000
+		if (i<=199) // 199 è il numero di volte che i multipli di 5 sono presenti sotto 1000
 				risultato += secondoMultiplo*i + primoMultiplo*i ;
 		else
 				risultato += primoMultiplo*i ;
@@ -67,9 +70,33 @@ public class stampa {
 		}
 	for (int i = 1; i<67; i++){
 		ripetizione += mcm*i;
-	}
+			}
+	int soluzione = risultato - ripetizione;
+		System.out.println("somma multipli di 3,5 minori di 1000: " + soluzione);
+		}
+	{
+			for (int i = 1; i<=33; i++){ 
+				if (i<=19)
+						risultato1 += secondoMultiplo*i + primoMultiplo*i ;
+				else
+						risultato1 += primoMultiplo*i ;
+					
+				}
+			for (int i = 1; i<7; i++){
+				ripetizione1 += mcm*i;
+					}
+			int soluzione = risultato1 - ripetizione1;
+				System.out.println("somma multipli di 3,5 minori di 100: " + soluzione);
+		}
+	{
+		for (int i = 1; i<=3; i++){ 
+			if (i<=1)
+					risultato2 += secondoMultiplo*i + primoMultiplo*i ;
+			else
+					risultato2 += primoMultiplo*i ;
+			}
+			System.out.println("somma multipli di 3,5 minori di 10: " + risultato2);
+		}
+	}	
+}
 
-		System.out.println(risultato - ripetizione);
-	}
-}
-}
